e6a83150b4789a470409aa2bfa8e6501432b2446
Skroot-Cameron <greenwalt@skrootlab.com>
2023-10-18 16:35:46 -0500

    - Added dialog to confirm or cancel software updates. - Added release notes to the confirm dialog

---
+++ release_notes.py
@@ -0,0 +1,102 @@
+import tkinter as tk
+import tkinter.ttk as ttk
+
+
+class ReleaseNotes:
+    def __init__(self, releaseNotes, root):
+        self.releaseNotes = releaseNotes
+        self.windowRoot = tk.Toplevel(root, bg='white', borderwidth=0, pady=25, padx=25)
+        self.windowRoot.maxsize(width=450, height=550)
+        scrollbar = tk.Scrollbar(self.windowRoot, orient=tk.VERTICAL, bg='white', borderwidth=0)
+        self.windowCanvas = tk.Canvas(
+            self.windowRoot, bg='white', yscrollcommand=scrollbar.set, width=400, height=500, borderwidth=0, highlightthickness=0
+        )
+        self.window = tk.Frame(self.windowRoot, bg='white', borderwidth=0)
+        self.windowCanvas.create_window(0, 0, anchor="nw", window=self.window)
+        row = self.fillInVersions()
+        self.createDownloadAndCancelButtons(row)
+        scrollbar.config(command=self.windowCanvas.yview)
+        self.windowCanvas.grid(row=0, column=0, sticky="ns")
+        self.windowCanvas.update()
+        self.window.update()
+        bounds = self.window.grid_bbox()
+        self.windowCanvas.configure(scrollregion=(0, 0, bounds[2] + 25, bounds[3] + 25))
+        scrollbar.grid(row=0, column=1, sticky="ns")
+        scrollbar.update()
+        self.windowCanvas.bind_all("<MouseWheel>", self.on_mousewheel)
+        self.download = False
+        root.wait_window(self.windowCanvas)
+
+    def fillInVersions(self):
+        row = 0
+        tk.Label(self.window, text="Release Notes", bg='white', font='Helvetica 14 bold').grid(row=row, column=0,
+                                                                                               sticky='w')
+        row += 1
+        ttk.Separator(self.window, orient='horizontal').grid(row=row, column=0, columnspan=2, sticky='ew')
+        row += 1
+        for version, notes in self.releaseNotes.items():
+            tk.Label(self.window, text=version, bg='white', font='Helvetica 12 bold').grid(row=row, column=0,
+                                                                                           sticky='w')
+            row += 1
+            if "features" in notes:
+                tk.Label(self.window, text="New Features:", bg='white', font='Helvetica 10 bold').grid(row=row,
+                                                                                                       column=0,
+                                                                                                       sticky='w')
+                row += 1
+                for index, feature in notes['features'].items():
+                    tk.Label(self.window, text=f"- {feature}", bg='white', font='Helvetica 10').grid(row=row, column=0,
+                                                                                                     sticky='w')
+                    row += 1
+                tk.Label(self.window, text='', bg='white', font='Helvetica 10').grid(row=row, column=0, sticky='w')
+                row += 1
+            if "bugfixes" in notes:
+                tk.Label(self.window, text="Bug Fixes:", bg='white', font='Helvetica 10 bold').grid(row=row, column=0,
+                                                                                                    sticky='w')
+                row += 1
+                for index, bugfix in notes['bugfixes'].items():
+                    tk.Label(self.window, text=f"- {bugfix}", bg='white', font='Helvetica 10').grid(row=row, column=0,
+                                                                                                    sticky='w')
+                    row += 1
+                tk.Label(self.window, text='', bg='white', font='Helvetica 10').grid(row=row, column=0, sticky='w')
+                row += 1
+            ttk.Separator(self.window, orient='horizontal').grid(row=row, column=0, columnspan=2, sticky='ew')
+            row += 1
+        return row
+
+    def createDownloadAndCancelButtons(self, row):
+        tk.Label(self.window, text='', bg='white', font='Helvetica 10').grid(row=row, column=0, sticky='w')
+        row += 1
+        downloadButton = ttk.Button(self.window, text="Download", command=lambda: self.setDownload(True))
+        downloadButton['style'] = 'W.TButton'
+        downloadButton.grid(row=row, column=0, sticky='w')
+        cancelButton = ttk.Button(self.window, text="Cancel", command=lambda: self.setDownload(False))
+        cancelButton['style'] = 'W.TButton'
+        cancelButton.grid(row=row, column=1, sticky='w')
+
+    def setDownload(self, value):
+        self.download = value
+        self.windowRoot.destroy()
+    def on_mousewheel(self, event):
+        scroll = -1 if event.delta > 0 else 1
+        self.windowCanvas.yview_scroll(scroll, "units")
+
+
+# root = tk.Tk()
+# ReleaseNotes({
+#     "v1.0.1": {
+#         "features": {"1": "Updated inoculation to zero out y-axis",
+#                      "2": "Updated plotting to use Skroot Growth Index (SGI)"},
+#         "bugfixes": {"1": "Fixed AWS Integrations"}},
+#     "v1.1.2": {
+#         "features": {"1": "Updated inoculation to zero out y-axis",
+#                      "2": "Updated plotting to use Skroot Growth Index (SGI)"},
+#         "bugfixes": {"1": "Fixed AWS Integrations"}},
+#     "v1.1.1": {
+#         "features": {"1": "Updated inoculation to zero out y-axis",
+#                      "2": "Updated plotting to use Skroot Growth Index (SGI)"},
+#         "bugfixes": {"1": "Fixed AWS Integrations"}},
+#     "v1.1.0": {
+#         "features": {"1": "Updated inoculation to zero out y-axis",
+#                      "2": "Updated plotting to use Skroot Growth Index (SGI)"},
+#         "bugfixes": {"1": "Fixed AWS Integrations"}}}, root)
+# root.mainloop()
---
+++ software_update.py
@@ -0,0 +1,75 @@
+import json
+import os
+
+import botocore
+
+import logger
+import release_notes
+from aws import AwsBoto3
+
+
+class SoftwareUpdate(AwsBoto3):
+    def __init__(self, root, major_version, minor_version, location):
+        super().__init__()
+        self.newestMajorVersion = major_version
+        self.newestMinorVersion = minor_version
+        self.location = location
+        self.root = root
+        self.newestZipVersion = ''
+        self.releaseNotesPrefix = 'release-notes'
+        with open('./resources/release-notes.json') as f:
+            self.releaseNotes = json.load(f)
+
+    def getCurrentReleaseNotes(self):
+        localFilename = f"{self.location}/v{self.newestMajorVersion}.{self.newestMinorVersion}.json"
+        self.downloadFile(
+            f"{self.releaseNotesPrefix}/v{self.newestMajorVersion}.{self.newestMinorVersion}.json",
+            localFilename
+        )
+        return localFilename
+
+    def mergeReleaseNotesIfNeededAndSave(self):
+        if f"v{self.newestMajorVersion}.{self.newestMinorVersion}" not in self.releaseNotes:
+            localFilename = self.getCurrentReleaseNotes()
+            with open(localFilename) as f:
+                dictionary = json.load(f)
+            self.releaseNotes.update(dictionary)
+            os.remove(localFilename)
+            with open(f"./resources/release-notes.json", "w") as outfile:
+                outfile.write(json.dumps(self.releaseNotes))
+
+    def downloadSoftwareUpdate(self, local_filename):
+        self.mergeReleaseNotesIfNeededAndSave()
+        ReleaseNotes = release_notes.ReleaseNotes(self.releaseNotes, self.root)
+        if ReleaseNotes.download:
+            self.downloadFile(self.newestZipVersion, local_filename)
+        return ReleaseNotes.download
+
+    def checkForSoftwareUpdates(self):
+        if not self.disabled:
+            allReleases = self.s3.list_objects_v2(Bucket='skroot-data', Prefix="software-releases")
+            for item in allReleases['Contents']:
+                filename = item['Key']
+                if filename == 'software-releases/':
+                    continue  # Don't try to get tags of the folder itself
+                try:
+                    tags = self.s3.get_object_tagging(Bucket='skroot-data', Key=filename)
+                    majorVersion = float(tags["TagSet"][0]['Value'])
+                    minorVersion = int(tags["TagSet"][1]['Value'])
+                except botocore.exceptions.ClientError:
+                    continue  # This means it's an R&D update and we are not using an R&D profile
+                except:
+                    logger.exception("failed to get tags of software update file")
+                if majorVersion > self.newestMajorVersion or (
+                        majorVersion == self.newestMajorVersion and minorVersion > self.newestMinorVersion):
+                    self.updateNewestVersion(majorVersion, minorVersion, filename)
+                    return f"{self.newestMajorVersion}.{self.newestMinorVersion}", True
+                else:
+                    return f"{self.newestMajorVersion}.{self.newestMinorVersion}", False
+        else:
+            return "", False
+
+    def updateNewestVersion(self, majorVersion, minorVersion, filename):
+        self.newestMajorVersion = majorVersion
+        self.newestMinorVersion = minorVersion
+        self.newestZipVersion = filename
