5eb06bedb4379b52f2a007fd1d615bb11c27bbff
awrice <68443158+awrice@users.noreply.github.com>
2023-12-09 12:57:55 -0700

    AWS resources tagging

--- resources/scripts/deployment_script.py
+++ resources/scripts/deployment_script.py
@@ -26,10 +26,13 @@ def zip_files(folder_path, zip_name):
                 arcname = os.path.relpath(file_path, folder_path)
                 zipf.write(file_path, arcname)
 
-def s3_upload_file(file_path, file_name, aws_folder_name):
+def s3_upload_file(file_path, file_name, aws_folder_name, tag_str=''):
     s3 = boto3.resource('s3')
     b = s3.Bucket('skroot-data')
-    b.upload_file(file_path, f'{aws_folder_name}/{file_name}')
+    b.upload_file(
+        file_path, f'{aws_folder_name}/{file_name}',
+        ExtraArgs={'Tagging': tag_str}
+    )
 
 with open('../version.json') as j_file:
     version = json.load(j_file)
@@ -41,16 +44,28 @@ zip_file_path = f'../temp/{zip_name}'
 release_notes_name = f'v{major_version}.{minor_version}.json'
 release_notes_fp = '../release-notes.json'
 
+software_releases_bucket = 'software-releases'
+release_notes_bucket = 'release-notes'
+
 try:
     # zip up the whole package
     print(f" > Zipping Files")
     zip_files('.', zip_file_path)
     # Upload the zip file to AWS in the skroot-data/software-releases bucket
     print(f" > Uploading Zip as '{zip_name}' from '{zip_file_path}'")
-    s3_upload_file(zip_file_path, zip_name, 'software-releases')
+    s3_upload_file(
+        zip_file_path, 
+        zip_name, 
+        software_releases_bucket, 
+        tag_str=f'major_version={major_version}&minor_version={minor_version}'
+    )
     # Upload the resources/release-notes.json file to the bucket skroot-data/release-notes
     print(f" > Uploading Release Notes as '{release_notes_name}' from '{release_notes_fp}'")
-    s3_upload_file(release_notes_fp, release_notes_name, 'release-notes')
+    s3_upload_file(
+        release_notes_fp, 
+        release_notes_name, 
+        release_notes_bucket
+    )
 except:
     traceback.print_exc()
 finally:
