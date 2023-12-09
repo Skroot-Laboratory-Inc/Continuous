6e186b54076d25f5c0f56fa14443d3d1f6a16767
awrice <68443158+awrice@users.noreply.github.com>
2023-12-09 12:39:14 -0700

    Update deployment script according to Cameron's notes

--- resources/scripts/deployment_script.py
+++ resources/scripts/deployment_script.py
@@ -29,31 +29,34 @@ def zip_files(folder_path, zip_name):
 def s3_upload_file(file_path, file_name, aws_folder_name):
     s3 = boto3.resource('s3')
     b = s3.Bucket('skroot-data')
-    res = b.upload_file(file_path, f'{aws_folder_name}/{file_name}')
+    b.upload_file(file_path, f'{aws_folder_name}/{file_name}')
 
-with open('./resources/version.json') as j_file:
+with open('../version.json') as j_file:
     version = json.load(j_file)
 
 major_version = version['major_version']
 minor_version = version['minor_version']
-zip_name = f"DesktopApp_v{major_version}.{minor_version}_TEST.zip"
-zip_file_path = f"../__TEST__/{zip_name}"
-release_notes_name = f'v{major_version}.{minor_version}_TEST.json'
-release_notes_fp = './resources/release-notes.json'
+zip_name = f'DesktopApp_v{major_version}.{minor_version}.zip'
+zip_file_path = f'../temp/{zip_name}'
+release_notes_name = f'v{major_version}.{minor_version}.json'
+release_notes_fp = '../release-notes.json'
+
 try:
     # zip up the whole package
-    print(f"===> Zipping Files")
+    print(f" > Zipping Files")
     zip_files('.', zip_file_path)
     # Upload the zip file to AWS in the skroot-data/software-releases bucket
-    print(f"===> Uploading Zip as '{zip_name}' from '{zip_file_path}'")
+    print(f" > Uploading Zip as '{zip_name}' from '{zip_file_path}'")
     s3_upload_file(zip_file_path, zip_name, 'software-releases')
     # Upload the resources/release-notes.json file to the bucket skroot-data/release-notes
-    print(f"===> Uploading Release Notes as '{release_notes_name}' from '{release_notes_fp}'")
+    print(f" > Uploading Release Notes as '{release_notes_name}' from '{release_notes_fp}'")
     s3_upload_file(release_notes_fp, release_notes_name, 'release-notes')
 except:
     traceback.print_exc()
 finally:
     # look for the zip file and delete it if it exists
     if os.path.exists(zip_file_path):
-        print("===> Deleting Zip")
+        print(" > Deleting Zip")
         os.remove(zip_file_path)
+
+
