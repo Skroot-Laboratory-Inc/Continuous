01392a22db3a3a99776c428cac8dd0d558051045
Skroot-Cameron <greenwalt@skrootlab.com>
2024-02-08 20:59:46 -0600

    Added gnome-screenshot to dependencies

--- resources/scripts/install-script.sh
+++ resources/scripts/install-script.sh
@@ -1,7 +1,7 @@
 #!bash
 sudo apt-get install python3-pip -y
 pip install -r ./requirements.txt
-sudo apt-get install python3-pil.imagetk libhidapi-dev scrot -y
+sudo apt-get install python3-pil.imagetk libhidapi-dev scrot gnome-screenshot -y
 sudo apt remove brltty -y
 sudo usermod -a -G dialout skroot
 sudo cp ../delete_old_files /etc/cron.d
