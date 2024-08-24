c0b18e8e460f36680bef318eff15c7eeae623786
Cameron <greenwalt@skrootlab.com>
2024-08-23 21:55:36 -0500

    SL-176 Fixed bug in running post_process_script on linux.

--- src/resources/scripts/post_process_script.py
+++ src/resources/scripts/post_process_script.py
@@ -1,6 +1,21 @@
+import os
+import sys
 import tkinter.filedialog
 
 from src.resources.scripts.post_processing.analyzer import PostProcessingAnalyzer
+
+try:
+    desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
+except KeyError:
+    desktop = os.path.join(os.path.join(os.path.expanduser('~')), 'Desktop')
+    sys.path = [
+        '/usr/lib/python3.10',
+        '/usr/lib/python3.10/lib-dynload',
+        '/usr/local/lib/python3.10/dist-packages',
+        '/usr/lib/python3/dist-packages',
+        '.',
+        '../../..',
+    ]
 
 print("Please select the experiment directory you would like to analyze.")
 experimentFolderDirectory = tkinter.filedialog.askdirectory()
