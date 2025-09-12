43906b6e8e1883a995a5c8211ec8d584a8075038
Cameron <greenwalt@skrootlab.com>
2025-09-12 15:14:58 -0500

    WW-238 Set the default flow rate to 5 mL/hr

--- src/resources/version/version.py
+++ src/resources/version/version.py
@@ -17,7 +17,7 @@ class UseCase(Enum):
 class Version:
     def __init__(self):
         self.majorVersion = 1.0
-        self.minorVersion = 0
+        self.minorVersion = 1
         self.useCase = UseCase.FlowCell
         self.developmentVersion = DevelopmentVersion.Test
 
