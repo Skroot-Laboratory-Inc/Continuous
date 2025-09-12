7d1cf0a082f435389e4a347ee808268ebae0343d
Cameron <greenwalt@skrootlab.com>
2025-09-12 15:26:42 -0500

    NOJIRA Updated calibration range to match scan range.

--- src/app/properties/sib_properties.py
+++ src/app/properties/sib_properties.py
@@ -1,7 +1,7 @@
 class SibProperties:
     def __init__(self):
-        self.calibrationStartFreq = 50
-        self.calibrationStopFreq = 170
+        self.calibrationStartFreq = 100
+        self.calibrationStopFreq = 160
         self.stepSize = 0.01
         self.initialSpikeMhz = 0.2
         self.yAxisLabel = 'Signal Strength (Unitless)'
