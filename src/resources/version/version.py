709f11b2ff986a7638ae26782f42a99325bcd5ed
Cameron <greenwalt@skrootlab.com>
2026-05-19 13:33:12 -0500

    WW-488 Added time in hours to the second axis CSV file that is generated.

--- src/resources/version/version.py
+++ src/resources/version/version.py
@@ -26,7 +26,7 @@ class Version:
 
     def __init__(self):
         self.major = 3.0
-        self.minor = 13
+        self.minor = 14
         self.developmentVersion = DevelopmentVersion.Test
         self.isBeta = True
 
